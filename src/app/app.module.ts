8646c9823555d2c5b6130a76cb297d42872c78bc
Felipe Francischetto <felipe-matteus@hotmail.com>
2017-11-17 23:18:06 -0200

    refactor: importing the components to the module and adding in the template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { WorkspaceComponent } from './components/workspace/workspace.component';
+import { CardComponent } from './components/workspace/card/card.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FormComponent } from './components/form/form.component';
+import { CardItemComponent } from './components/workspace/card/card-item/card-item.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorkspaceComponent,
+    CardComponent,
+    NavbarComponent,
+    FormComponent,
+    CardItemComponent
   ],
   imports: [
     BrowserModule
