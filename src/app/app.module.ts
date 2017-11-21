841adbc287261df06a37b69b1baddfabcfc95eaf
Felipe Francischetto <felipefrancischetto@hotmail.com>
2017-11-21 17:55:14 -0200

    chore: adding the style properties of the Dragula library.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
+import { DragulaModule } from 'ng2-dragula/components/dragular.module';
 
 import { AppComponent } from './app.component';
 import { WorkspaceComponent } from './components/workspace/workspace.component';
@@ -8,21 +10,24 @@ import { CardComponent } from './components/workspace/card/card.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FormComponent } from './components/form/form.component';
 import { CardItemComponent } from './components/workspace/card/card-item/card-item.component';
-
+import { CardService } from './components/workspace/card/card.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WorkspaceComponent,
     CardComponent,
-    NavbarComponent,
+    CardItemComponent,
     FormComponent,
-    CardItemComponent
+    NavbarComponent,
+    WorkspaceComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    DragulaModule
   ],
-  providers: [],
+  providers: [CardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
